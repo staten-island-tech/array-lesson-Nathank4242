a02aa40ecf8fa4d9cb811c390bff7586ee09cca0
Nathank4242 <nkweon@gmail.com>
2024-10-08 13:57:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,59 @@ students.forEach((student) => console.log(student.name + ":" + student.grade));
 
 // 1. Log each student’s name and grade
 
-const topStudents = students.filter((student) => student.grade > 80);
-students.filter.forEach((student) => console.log(student.name));
+const goodStudent = students.filter((student) => student.grade > 80);
 
-console.log(topStudents);
+console.log(goodStudent);
 
-const youngStudents = students.filter((student) => student.age >= 21);
-students.filter.forEach((student) => console.log(student.name));
+const youngStudents = students.filter((student) => student.age <= 21);
+
+const youngStudentname = students
+  .filter((student) => student.age <= 21)
+  .forEach((student) => console.log(student.name + ":" + student.age));
+
+console.log(youngStudents:);
+console.log(goodStudent);
+
+//product arrays
+
+const products = [
+  { name: "Laptop", price: 1200, category: "Electronics", rating: 4.5 },
+  { name: "Phone", price: 800, category: "Electronics", rating: 4.7 },
+  { name: "Headphones", price: 150, category: "Accessories", rating: 4.3 },
+  { name: "Monitor", price: 300, category: "Electronics", rating: 4.6 },
+  { name: "Keyboard", price: 100, category: "Accessories", rating: 4.1 },
+  { name: "Chair", price: 250, category: "Furniture", rating: 4.0 },
+  { name: "Desk", price: 450, category: "Furniture", rating: 4.8 },
+];
+
+const namePrice = products.forEach((product) =>
+  console.log(product.name + ":" + product.price)
+);
+
+const updatePrice = products.forEach((product) =>
+  console.log(Math.round(product.price * 1.1))
+);
+console.log(updatePrice);
+
+const categories = [];
+products.forEach((product) => {
+  if (!categories.includes(product.category)) {
+    categories.push(product.category);
+  }
+});
+
+console.log("Unique Categories:", categories);
+
+//part 2
+
+const electronics = products.filter(
+  (product) => product.category === "Electronics"
+);
+console.log("Electronics:", electronics);
+
+const expensiveProducts = products.filter((product) => product.price > 300);
+console.log("Expensive Products:", expensiveProducts);
+
+const rating = products
+  .filter((student) => student.age <= 21)
+  .forEach((student) => console.log(student.name + ":" + student.age));
